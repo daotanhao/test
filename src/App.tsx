c1dd5bd78faf55563a9dca3a2b2b57155bdcf034
Đào Tấn Hào <84759921+daotanhao@users.noreply.github.com>
2024-07-16 22:19:16 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-// import './App.css'
-import DataSourceDetails from './components/DataSourceDetails'
-import DataDisplay from './components/DataDisplay'
+import React, { useState } from 'react'
+import {DataTable} from './components/DataTable'
 
-function App() {
+const App: React.FC = () => {
+  const base64Data = 'SGVsbG8gd29ybGQh'; // Dữ liệu base64 mẫu
+  const handleDataChange = (updatedBytes: number[]) => {
+    console.log('Updated Bytes:', updatedBytes);
+  };
+
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: window.innerWidth }}>
-      <div style={{ display: 'flex', width: '50%' }}>
-        <DataSourceDetails />
-      </div>
-      <div style={{ display: 'flex', width: '50%' }}>
-        <DataDisplay />
-      </div>
-    </div>
+    <DataTable base64Data={base64Data} onChange={handleDataChange} />
   )
 }
 
